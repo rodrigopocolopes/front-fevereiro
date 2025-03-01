4f2e07030f95f7913eb41fab688e61c5b7ce6c97
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-02-28 21:22:38 -0300

    form jquery

---
+++ form.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    $("#bt-cadastrar").click(function () {
+
+        $("input, select").removeClass("is-invalid");
+        $("label").removeClass("text-danger fw-bold");
+
+        var nome = $("#input-nome").val();
+
+        if (nome.trim().length <= 1) {
+
+            $("#input-nome").addClass("is-invalid");
+            $("#label-nome").addClass("text-danger fw-bold");
+        }
+
+        var tel = $("#input-tel").val();
+        if (tel.trim().length < 10) {
+            $("#input-tel").addClass("is-invalid");
+            $("#label-tel").addClass("text-danger fw-bold");
+        }
+
+        var email = $("#input-email").val();
+        if (email.trim() == "") {
+            $("#input-email").addClass("is-invalid");
+            $("#label-email").addClass("text-danger fw-bold");
+        }
+
+        var city = $("#input-city").val();
+        if (city.trim() == "") {
+            $("#input-city").addClass("is-invalid");
+            $("#label-city").addClass("text-danger fw-bold");
+        }
+
+        var estado = $("#input-state").val();
+        if (estado == "selecione") {
+            $("#input-state").addClass("is-invalid");
+            $("#label-state").addClass("text-danger fw-bold");
+        }
+
+        var masc = $("#input-masc").val();
+        var fem = $("#input-fem").val();
+        var sexo = $("[name=sexo]:checked").val()
+        if (sexo == undefined) {
+            $("#input-masc").addClass("is-invalid");
+            $("#input-fem").addClass("is-invalid");
+            $("#label-sexo").addClass("text-danger fw-bold");
+
+        }
+
+
+    }) // fim do bt-cadastrar
+
+    $("#input-tel").keydown(function (ev) {
+
+        console.log(ev.key);
+        if (ev.keyCode >= 48 && ev.keyCode <= 57 || ev.keyCode == 32) {
+
+        } else {
+            return false;
+        }
+
+    }) // fim do keydown
+
+})
